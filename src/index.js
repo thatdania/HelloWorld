0439e7e9f25117ff0bc3cbab864e6f6e24fcbeac
thatdania@gmail.com <thatdania@gmail.com>
2018-01-24 21:26:20 +0000

    Add Hello world without looking at instructions

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <center>
+   <h1>Hello World</h1>
+  </center>,
+  document.getElementById('root'));
 registerServiceWorker();
